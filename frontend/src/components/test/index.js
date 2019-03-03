2afc47607553d18ef46aeadfefa08b44373ad8a2
shadowvzs <shadowvzs@hotmail.com>
2019-03-03 08:32:01 +0200

    cleaning with hook separation

--- frontend/src/components/test/index.js
+++ frontend/src/components/test/index.js
@@ -1,10 +1,13 @@
-import React, { useState, useEffect } from 'react';     //useContext for context
+import React, { useState, useEffect, useReducer } from 'react';     //useContext for context
 
 export default function Greetings(props) {
     console.log('render Greetings');
     const name = useFormInput("Pista");
     const surname = useFormInput("Nagy");
     const width = useWindowWidth();
+    const small = useMedia("(max-width: 400px)");
+    const large = useMedia("(min-width: 800px)");
+
     //const theme = useContext(ThemeContext);           //ThemeContext is imported from our context file
     useDocumentTitle(name.value + ' ' + surname.value);
 
@@ -17,7 +20,9 @@ export default function Greetings(props) {
             <input
                 {...surname}
             />
-            <small>{width}</small>            
+            <small>{width} </small>
+            <p> Small { small ? 'Yes' : 'No' } </p>       
+            <p> Large { large ? 'Yes' : 'No' } </p>       
         </div>
     );
 }
@@ -45,7 +50,7 @@ function useDocumentTitle(title) {
 
 // this is a custom hooks which is reuseable and separated because we make simplier our Greetings function
 // custom hooks should have "use" name prefix (u can know if function name start with "use" then probabil got internal state)
-function useWindowWidth() {
+function useWindowWidth(dispatch) {
     const [width, setWidth] = useState(window.innerWidth);
     useEffect(() => {
         const resize = () => setWidth(window.innerWidth);
@@ -59,3 +64,56 @@ function useWindowWidth() {
     });
     return width;
 }
+
+// this function make a checks, normally we must nest at each condition
+// but with hooks we can solve different way
+// ex. window.matchMedia("(min-width: 800px)").matches is true or false
+function useMedia(query) {
+    const media = window.matchMedia(query);
+    const [matches, setMatches] = useState(media.matches);
+
+    // useEffect got 2 param: 
+    // - 1. is a function which execute what we need
+    // - 2. is input param, like previous state or prop
+
+    useEffect( () => {
+        // init
+        if (matches !== media.matches) {
+            setMatches(media.matches);
+        }
+        // componentDidMount part
+        const listener = () => setMatches(media.matches);
+        media.addListener(listener);
+        // componentWillUnmount
+        return () => media.removeListener(listener);
+        // query is input for useffect, like prevProps at componentDidUpdate
+    }, [query])
+
+    return matches;
+}
+
+// use effect with timer also tricky because we must clean up the timer
+// this not let slideshow move foward after you paused or when rerendered
+/*
+useEffect(() => {
+
+    if (isPlaying) {
+        let timer = setTimoutout( () => {
+            setCurrentIndex(currentIndex++);
+        }, 2000)
+
+        return () => clearTimeout(timer);
+    }
+}, [currentIndex, isPlaying])
+
+*/
+
+// key is used to reset element
+/*
+    <ProgressBar
+        key={currentIndex + isPlaying}
+        time={SLIDE_DURATION}
+        animate={isPlaying}
+    />
+
+*/
